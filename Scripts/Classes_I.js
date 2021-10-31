2549e9ec2df9e2a28863b61c72bcfca387bb73c0
Gasegamer <66225865+Gasegamer@users.noreply.github.com>
2021-10-31 11:27:33 -0300

    Split Classes.js script

---
+++ Scripts/Classes_I.js
@@ -0,0 +1,332 @@
+class Inventory extends Sprite{
+	constructor(img, layer, type){
+		super(img, 0, 0, 892, 512, 0, 0, 1000, 600, layer, type);
+		this.layer = layer;
+		this.type = type;
+		this.isOpen = false;
+		this.closeButton = new Button(870, 30, 65, 86);
+		this.bioButton = new Button(355, 505, 60, 50);
+		this.isChanging = false;
+		this.bigBird = {x: this.x + 150, y: this.y + 150};
+		this.bigBird.img = new Image();
+		this.bigBird.shadowImg = new Image();
+		this.bigBird.img.src = hudSrc + "big_bird.png";
+		this.bigBird.shadowImg.src = hudSrc + 'big_bird_shadow.png';
+		this.canDrawBigBird = false;
+		this.canDrawGrid = true;
+		this.canDrawBio = false;
+	}
+
+	open(){
+		if(this.isOpen == false){
+			this.isChanging == false ? this.getInventory() : this.createItemsButtons();
+			this.isOpen = true;
+			localPlayer.canMove = false;
+			this.canDrawBigBird = false;
+			this.canDrawGrid = true;
+			this.canDrawBio = false;
+		}
+	} 
+
+	close(){
+		if(this.closeButton.isInsideButton(mousePos) == true){
+			this.isChanging = true;
+			command('/updateInventory', this.items);
+			this.updateGear();
+			this.isOpen = false;
+			localPlayer.canMove = true;
+		}
+	}
+
+	getInventory(){
+		PlayFabClientSDK.GetUserInventory({SessionTicket: sessionStorage.ticket}, (result, error) =>{
+			if(result !== null){
+				this.items = result.data.Inventory;
+				this.createItemsButtons();
+			}else if(error !== null){
+				console.log(error);
+			}
+		})
+	}
+
+	createItemsButtons(){
+		let pastX = 513;
+		let pastY = 132;
+		for(let i = 0; i < this.items.length; i++){
+			if(i % 4 == 0 && i != 0){
+				pastX = 507;
+				pastY += 77.5;
+			}
+			this.items[i].button = new Button(pastX, pastY, 85, 77.5);
+			if(this.items[i].CustomData.isEquipped == 'true') this.items[i].button.isSelected = true;
+			pastX += 95;
+		}
+		this.canDrawItems = true;
+	}
+
+	updateGear(){
+		localPlayer.items = this.items;
+		localPlayer.itemsImgs = new Array();
+		let colors = new Array();
+		localPlayer.items.forEach((item) => {
+			if(item.CustomData.isEquipped == 'true'){
+				localPlayer.addItem(item.ItemClass, item.ItemId);
+			}
+			if(item.ItemClass == 'color'){
+				let colorItem = {ItemId: item.ItemId, isEquipped: item.CustomData.isEquipped}
+				colors.push(colorItem);
+			}
+		})
+		if(getElementFromArrayByValue('true', 'isEquipped', colors) == false){
+			let tempCharacterImg = new Image();
+			tempCharacterImg.src = charactersSrc + "bird_blue.png";
+			localPlayer.img = tempCharacterImg;
+			localPlayer.img.name = 'bird_blue';
+		}
+	}
+
+	drawSquares(pastX, pastY, squareHeight, array, callback){
+		for(let i = 0; i < array.length; i++){
+			if(i % 4 == 0 && i != 0){
+				pastX = 511;
+				pastY += squareHeight;
+			}
+			callback(array, i, pastX, pastY);
+			pastX += 95;
+		}
+	}
+
+	drawItems(items, i, pastX, pastY){
+		items[i].img = new Image();
+		items[i].img.src = itemsSrc + items[i].ItemClass + "/" + items[i].ItemId + "_icon.png";
+		ctx.fillStyle = "#bab6aa";
+		ctx.fillRect(pastX - 4, pastY, 95, 85); //draws the grey rectangle
+		ctx.drawImage(items[i].img, pastX, pastY + 2);
+		
+		if(items[i].button.isSelected == true){
+			ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+			ctx.fillRect(pastX - 4, pastY - 3, 95, 85); //draws the grey rectangle
+		}
+
+		items[i].button.isOverButton(mouseOver) == true ? items[i].button.isOver = true : items[i].button.isOver = false;
+
+		if(items[i].button.isOver == true){
+			ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+			ctx.fillRect(pastX - 4, pastY, 95, 85); //draws the grey select rectangle
+		}
+	}
+
+	selectItem(){
+		this.items.forEach(item =>{
+			item.button.isInsideButton(mousePos);
+			if(item.button.isSelected == true){
+				item.CustomData.isEquipped = "true";
+				this.items.forEach(fItem =>{
+					if(fItem.ItemClass == item.ItemClass && fItem.ItemId != item.ItemId && fItem.CustomData.isEquipped == "true"){
+						fItem.CustomData.isEquipped = "false";
+						fItem.button.isSelected = false;
+					}
+				})
+			}else{
+				item.CustomData.isEquipped = "false";
+			}
+		})
+	}
+
+	drawGrid(){
+		let pastX = 507;
+		let pastY = 127;
+		let squareWidth = 95;
+		let squareHeight = 87.5;
+		for(let i = 0; i < 16; i++){
+			if(i % 4 == 0 && i != 0){
+				pastX = 507;
+				pastY += squareHeight;
+			}
+			ctx.beginPath();
+			ctx.rect(pastX, pastY, squareWidth, squareHeight);
+			ctx.strokeStyle = "black";
+			ctx.stroke();
+			pastX += 95;
+		}
+	}
+
+	drawWhiteRectangle(){
+		if(this.canDrawItems == true){
+			let pastX = 511;
+			let pastY = 130;
+			for(let i = 0; i < this.items.length; i++){
+				if(i % 4 == 0 && i != 0){
+					pastX = 511;
+					pastY += 87.5;
+				}
+				if(this.items[i].button.isSelected == true){
+					ctx.beginPath();
+					ctx.strokeStyle = "white";
+					ctx.lineWidth = 6;
+					ctx.rect(pastX - 4, pastY - 3.2, 95, 87.5); //draws the white rectangle
+					ctx.stroke();
+				}
+				pastX += 95;
+			}
+		}
+	}
+	
+	grayCloseButton(){
+		ctx.fillStyle = "rgba(0, 0, 0, 0.4)"
+		ctx.beginPath();
+		ctx.moveTo(this.closeButton.x, this.closeButton.y);
+		ctx.lineTo(this.closeButton.x + 66,this.closeButton.y);
+		ctx.lineTo(this.closeButton.x + 70, 100);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.moveTo(this.closeButton.x, this.closeButton.y);
+		ctx.bezierCurveTo(this.closeButton.x, this.closeButton.y + 15,this.closeButton.x + 35, this.closeButton.y + 60, this.closeButton.x + 70, 100);
+		ctx.fill();
+	}
+
+	drawBigBird(){
+		ctx.drawImage(this.bigBird.shadowImg, this.bigBird.x, this.bigBird.y + 230);
+		ctx.drawImage(this.bigBird.img, this.bigBird.x, this.bigBird.y);
+		this.bigBird.items = this.items;
+		this.bigBird.gear = new Array();
+		this.bigBird.colors = new Array();
+		this.bigBird.items.forEach(item =>{
+			if(item.button.isSelected == true){
+				item.img = new Image();
+				item.img.src = itemsSrc + item.ItemClass + '/' + item.ItemId + '_big.png';
+				let imgX, imgY, imgLayer;
+				let canPush = false;
+				switch(item.ItemClass){
+					case 'hand':
+						imgX = this.bigBird.x;
+						imgY = this.bigBird.y;
+						canPush = true
+						imgLayer = 6;
+						break;
+					case 'head':
+						imgX = this.bigBird.x + 14;
+						imgY = this.bigBird.y - 14;
+						canPush = true;
+						imgLayer = 5;
+						break;
+					case 'face':
+						imgX = this.bigBird.x + 30;
+						imgY = this.bigBird.y + 47;
+						canPush = true;
+						imgLayer = 4;
+						break;
+					case 'neck':
+						imgX = this.bigBird.x + 75;
+						imgY = this.bigBird.y + 150;
+						canPush = true
+						imgLayer = 3;
+						break;
+					case 'body':
+						imgX = this.bigBird.x;
+						imgY = this.bigBird.y;
+						canPush = true
+						imgLayer = 2;
+						break;
+					case 'feet':
+						imgX = this.bigBird.x;
+						imgY = this.bigBird.y;
+						canPush = true
+						imgLayer = 1;
+						break;
+					case 'color':
+						this.bigBird.img.src = item.img.src;
+						this.bigBird.colors.push(item);
+						canPush = false;
+						imgLayer = 0;
+						break;
+				}
+				if(canPush == true){this.bigBird.gear.push({i: item.img, x: imgX, y: imgY, layer: imgLayer});}
+			}
+		})
+		this.bigBird.gear.sort((b, a) => {return b.layer - a.layer});
+		this.bigBird.gear.forEach((item) =>{
+			ctx.drawImage(item.i, item.x, item.y);
+		})
+		if(this.bigBird.colors.length > 0){
+			this.bigBird.colors.forEach((item)=>{
+				if(item.button.isSelected == false){
+					removeElementFromArray(item, this.bigBird.colors);
+				}
+			})
+		}else{
+			this.bigBird.img.src = hudSrc + "big_bird.png";
+		}
+	}
+
+	drawUsername(){
+		if(localPlayer.username.length > 16){
+			ctx.font = '46px Caslon';
+		}else{
+			ctx.font = '55px Caslon';
+		}
+		ctx.fillStyle = '#615f5b';
+		ctx.fillText(localPlayer.username, this.x + 260, this.y + 80);
+	}
+
+	writeBio(){
+		this.bioButton.isInsideButton(mousePos);
+		if(this.bioButton.isSelected == true){
+			this.canDrawItems = false;
+			this.canDrawGrid = false;
+			this.canDrawBio = true;
+			this.saveBio();
+			bioInput.hidden = false;
+			if(localPlayer.bio != "I like to play Birdpals!"){
+				bioInput.value = localPlayer.bio;
+			}
+		}else{
+			this.canDrawItems = true;
+			this.canDrawGrid = true;
+			this.canDrawBio = false;
+			bioInput.hidden = true;
+			bioInput.disabled = false;
+		}
+	}
+	saveBio(){
+		bioInput.onkeyup = function(evt) {
+            evt = evt || window.event;
+
+            if (evt.keyCode == 13) {
+				 bioInput.disabled = true;
+				 command('/changeBio', bioInput.value);
+				 localPlayer.bio = bioInput.value;
+            }
+        };
+	}
+	customDraw(){
+		try {
+			if(this.isOpen == true){
+				if(this.closeButton.isOver == true){
+					this.grayCloseButton();
+				}
+				if(this.canDrawItems == true){
+					this.drawSquares(511, 130, 87, this.items, this.drawItems);
+					this.canDrawBigBird = true;
+				}
+				if(this.canDrawBigBird == true){
+					this.drawBigBird();
+				}
+				this.drawUsername();
+				if(this.canDrawGrid == true){
+					this.drawGrid();
+				}
+				if(this.canDrawBio == true){
+					ctx.beginPath();
+					ctx.rect(507, 130, 380, 350);
+					ctx.strokeStyle = "black";
+					ctx.stroke();
+				}
+				this.drawWhiteRectangle();
+			}
+		} catch (error) {
+			console.error(error)
+		}
+		
+	}
+}
---
+++ Scripts/States.js
@@ -0,0 +1,462 @@
+class State{
+    constructor(){
+
+    }
+
+    onclick(evt){
+
+    }
+    onmousemove(evt){
+
+    }
+
+    main(){
+
+    }
+
+    render(){
+
+    }
+}
+
+var allObjects;
+class WorldState extends State{
+    constructor(){
+        super();
+    }
+	
+    render(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        roomSprite.draw();
+        allObjects = [];
+        allObjects = playersObject.concat(roomObjects, localPlayer, roomNPCs);
+
+        allObjects.sort(function(a, b){return a.y-b.y});
+
+        allObjects.forEach((object) => {
+            if(object != undefined){
+                object.draw();
+            }
+        });
+
+		foreground.draw();
+
+        if(playersObject.length > 0){
+            playersObject.forEach((player) => {
+                player.whereToLook();
+                player.drawUsername();
+                player.drawBubble();
+				if(player.card.isOpen == true){
+					ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+					ctx.fillRect(0,0,canvas.width, canvas.height);
+					player.card.draw();
+				}
+            });
+        }
+
+        if(roomNPCs != undefined && roomNPCs.length > 0){
+            roomNPCs.forEach((npc)=>{
+                npc.drawName();
+            })
+        }
+
+        if(localPlayer != undefined){
+			if(localPlayer.canDrawUsername == true){
+				localPlayer.drawUsername();
+			}
+            localPlayer.drawBubble();
+        }
+
+		if(inventory != undefined && inventory.isOpen == true){
+			ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+			ctx.fillRect(0,0,canvas.width, canvas.height);
+			inventory.draw();
+		}else{
+			bioInput.hidden = true;
+		}
+
+        this.customrender();
+    }
+
+    customrender(){
+
+    }
+
+    onclick(evt){
+        mousePos = getMousePos(canvas, evt);
+        if(localPlayer != undefined && localPlayer.canMove == true){
+            localPlayer.mouseX = mousePos.x;
+            localPlayer.mouseY = mousePos.y;
+            localPlayer.move();
+			const playerMovement = {
+				mouseX: mousePos.x,
+				mouseY: mousePos.y
+			}
+			socket.emit('playerMovement', playerMovement);
+		}else if(inventory != undefined && inventory.isOpen == true){
+			inventory.close();
+			inventory.selectItem();
+			inventory.writeBio();
+		}
+		playersObject.forEach((tempplayer) =>{
+            if(tempplayer.card != undefined && tempplayer.card.playerButton.isInsideButton(mousePos) == true && tempplayer.card.isOpen == false){
+                if(localPlayer.isMoving == true){
+                    localPlayer.isMoving = false;
+                    clearInterval(localPlayer.movePlayerInterval);
+                }
+                tempplayer.card.open();
+            }else if(tempplayer.card != undefined && tempplayer.card.closeButton.isInsideButton(mousePos) == true && tempplayer.card.isOpen == true){
+                tempplayer.card.close();
+            }
+        });
+	}
+
+	onmousemove(evt){
+		mouseOver = getMousePos(canvas, evt);
+		if(inventory != undefined && inventory.isOpen == true){
+			inventory.closeButton.isOverButton(mouseOver) == true ? inventory.closeButton.isOver = true : inventory.closeButton.isOver = false;
+		}
+	}
+	
+}
+
+class JukeboxState extends State{
+    constructor(){
+        super();
+        this.tracks = [];
+        customGetJSON(JSONSrc + 'jukeboxJSON.json').then(response =>{
+            this.jukeboxJSON = response;
+            for(let i in this.jukeboxJSON){
+                this.tracks.push([this.jukeboxJSON[i].name, this.jukeboxJSON[i].purpose, this.jukeboxJSON[i].author, this.jukeboxJSON[i].file]);
+            }
+        })
+        stopMusic();
+    }
+
+    render(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for(let i = 0; i < this.tracks.length; i++){
+            ctx.beginPath();
+            ctx.lineWidth = "6";
+            ctx.strokeStyle = "black";
+            ctx.rect(10, 10+i*110, 400, 100);
+            ctx.stroke();
+            ctx.fillStyle = "white";
+            ctx.fill();
+            ctx.fillStyle = "black";
+            ctx.textAlign = "left";
+            ctx.font = "30px Arial";
+            ctx.fillText(this.tracks[i][0], 20, 40+i*110);
+            ctx.font = "20px Arial";
+            ctx.fillText(this.tracks[i][1], 20, 100+i*110);
+            ctx.fillText(this.tracks[i][2], 200, 100+i*110);
+        }
+        ctx.beginPath();
+        ctx.lineWidth = "6";
+        ctx.strokeStyle = "black";
+        ctx.rect(900, 10, 90, 50);
+        ctx.stroke();
+        ctx.fillStyle = "white";
+        ctx.fill();
+        ctx.strokeStyle = "black";
+        ctx.font = "20px Arial";
+        ctx.fillText("Back", 920, 40);
+    }
+
+    onclick(evt){
+        let mousePos = getMousePos(canvas, evt);
+        if(mousePos.x <= 990 && mousePos.x >= 900 && mousePos.y <= 60 && mousePos.y >= 10){
+            currentState = new WorldState();
+            background_music.src = audioSrc + rooms[currentRoom].music;
+		    currentMusicSrc = audioSrc + rooms[currentRoom].music;
+            return;
+        }
+        for(let i = 0; i < this.tracks.length; i++){
+            if(mousePos.x <= 400 && mousePos.x >= 10 && mousePos.y <= 10+i*110+100 && mousePos.y >= 10+i*110){
+                let src = this.tracks[i][3];
+                background_music.src = audioSrc + src;
+		        currentMusicSrc = audioSrc + src;
+                return;
+            }
+        }
+    }
+}
+
+class TableTennisState extends State{
+    constructor(){
+        super();
+        this.ballX = canvas.width/2;
+        this.ballY = 0;
+        this.ballVelX = 0;
+        this.ballVelY = 10;
+        this.ballGoingDown = true;
+
+        let paddleSprite = loadSprite("Sprites/minigames/tabletennis/Paddle.png");
+        let tableSprite = loadSprite("Sprites/minigames/tabletennis/table.png");
+        this.player1Paddle = new Sprite(paddleSprite, 0, 0, 224, 245, 0, 400, 224, 245, 110, 0);
+        this.table = new Sprite(tableSprite, 0, 0, 1000, 600, 0, 0, 1000, 600, 0, 0);
+    }
+
+    main(){
+        if(this.ballGoingDown){
+            if(this.ballY >= 500){
+                this.ballGoingDown = false;
+                this.ballVelY *= -1
+            }
+        }
+        else{
+
+        }
+
+        this.ballX += this.ballVelX;
+        this.ballY += this.ballVelY;
+    }
+
+    render(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.table.draw();
+        var c = document.getElementById("myCanvas");
+        ctx.beginPath();
+        ctx.arc(this.ballX, this.ballY, 10, 0, 2 * Math.PI);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        this.player1Paddle.draw();
+    }
+
+    onmousemove(evt){
+        let tempMousePos = getMousePos(canvas, evt);
+        this.player1Paddle.x = tempMousePos.x;
+    }
+}
+
+class BirdInvadersState extends State{
+    constructor(){
+        super();
+
+        let machineimg = new Image();
+        machineimg.src = "Sprites/minigames/BirdInvaders/machine.png";
+        this.machineSprite = new Sprite(machineimg, 0, 0, 1000, 600, 0, 0, 1000, 600, 0, 0);
+        let buttonsimg = new Image();
+        buttonsimg.src = "Sprites/minigames/BirdInvaders/buttons.png";
+        this.buttonsSprite = new Sprite(buttonsimg, 0, 0, 1000, 600, 0, 0, 1000, 600, 0, 0);
+
+        this.customplayer = {x:100, y:100, width:82, height:110, username:"", id:"", bio:"", items:[], mouseX:0, mouseY:0, isDev:false, isMoving:false};
+
+        this.player = new Player(this.customplayer);
+        this.player.img.src = "Sprites/minigames/BirdInvaders/Birds/pixel_blue.png";
+
+        collisionArray = [];
+    }
+
+    render(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+
+        this.machineSprite.draw();
+        this.player.draw();
+        this.buttonsSprite.draw();
+    }
+
+    onclick(evt){
+        mousePos = getMousePos(canvas, evt);
+        if(this.player != undefined && this.player.canMove == true){
+            this.player.mouseX = mousePos.x;
+            this.player.mouseY = mousePos.y;
+            this.player.move();
+			const playerMovement = {
+				mouseX: mousePos.x,
+				mouseY: mousePos.y
+			}
+		}else if(inventory != undefined && inventory.isOpen == true){
+			inventory.close();
+			inventory.selectItem();
+			inventory.writeBio();
+		}
+	}
+}
+
+
+//Debug States
+
+class DebugWorldState extends WorldState{
+    constructor(){
+        super();
+    }
+
+    onclick(evt){
+        mousePos = getMousePos(canvas, evt);
+        if(localPlayer != undefined){
+            localPlayer.mouseX = mousePos.x;
+            localPlayer.mouseY = mousePos.y;
+            localPlayer.move();
+        }
+        const playerMovement = {
+            mouseX: mousePos.x,
+            mouseY: mousePos.y
+        }
+        socket.emit('playerMovement', playerMovement);
+
+        debugParagraph.innerHTML = "mouse X: " + mousePos.x.toString() + " mouse Y: " + mousePos.y.toString();
+    }
+
+    customrender(){
+        drawCollisionMap();
+    }
+}
+
+class DebugRoomState extends State{
+    constructor(roombackgroundsrc, roomdetailssrc){
+        super();
+        
+        this.roombackgroundimg = new Image();
+        this.roombackgroundimg.src = roomsSrc + roombackgroundsrc;
+
+        this.roomdetailsimg = new Image();
+        this.roomdetailsimg.src = roomsSrc + roomdetailssrc;
+
+        this.tempmousepos = {x:0,y:0};
+        this.currentclick = 0;
+        this.click1 = [0, 0];
+        this.click2 = [0, 0];
+        this.click3 = [0, 0];
+    }
+
+    render(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+
+        ctx.drawImage(this.roombackgroundimg, 0, 0);
+        ctx.drawImage(this.roomdetailsimg, 0, 0);
+
+        if(this.currentclick == 0){
+            ctx.beginPath();
+            ctx.rect(this.tempmousepos.x, this.tempmousepos.y, 10, 10);
+            ctx.stroke();
+        }
+        else if(this.currentclick == 1){
+            ctx.beginPath();
+            ctx.rect(this.click1[0], this.click1[1], this.tempmousepos.x - this.click1[0], this.tempmousepos.y - this.click1[1]);
+            ctx.stroke();
+        }
+        else if(this.currentclick == 2){
+            ctx.beginPath();
+            ctx.rect(this.click1[0], this.click1[1], this.click2[0], this.click2[1]);
+            ctx.rect(this.tempmousepos.x, this.tempmousepos.y, 10, 10);
+            ctx.stroke();
+        }
+    }
+
+    onmousemove(evt){
+        this.tempmousepos = getMousePos(canvas, evt);
+        ctx.lineWidth = "1";
+    }
+
+    onclick(evt){
+        let objectString = "";
+        if(this.currentclick == 0){
+            this.click1 = [this.tempmousepos.x, this.tempmousepos.y];
+        }
+        else if(this.currentclick == 1){
+            this.click2 = [this.tempmousepos.x - this.click1[0], this.tempmousepos.y - this.click1[1]];
+        }
+        else if(this.currentclick == 2){
+            this.click3 = [this.tempmousepos.x - this.click1[0], this.tempmousepos.y - this.click1[1]];
+            objectString = "["+this.click1[0].toString()+","+this.click1[1].toString()+","+this.click2[0].toString()+","+this.click2[1].toString()+","+this.click3[0].toString()+","+this.click3[1].toString()+"]";
+
+            console.log(objectString);
+
+        }
+        else if(this.currentclick == 3){
+            this.currentclick = -1;
+            this.click1 = [0, 0];
+            this.click2 = [0, 0];
+            this.click3 = [0, 0];
+        }
+
+        this.currentclick += 1;
+    }
+}
+
+class DebugSkinEditorState extends State{
+    constructor(skinimg){
+        super();
+        
+        this.itemimg = skinimg;
+        this.click1 = [0,0];
+        this.click2 = [0,0];
+        this.currentclick = 0;
+
+        this.spritesArray = [];
+
+        this.drawSpritesheetthingy();
+    }
+
+    drawSpritesheetthingy() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(this.itemimg, 0, 0);
+    }
+
+    onclick(evt){
+        let tempmousepos = getMousePos(canvas, evt);
+        if(this.currentclick == 0){
+            this.click1 = [this.tempmousepos.x, this.tempmousepos.y];
+            this.currentclick = 1;
+            this.drawSpritesheetthingy();
+        }
+        else if(this.currentclick == 1){
+            this.click2 = [this.tempmousepos.x - this.click1[0], this.tempmousepos.y - this.click1[1]];
+            this.currentclick = 2;
+            //ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.beginPath();
+            ctx.rect(this.click1[0], this.click1[1], this.click2[0], this.click2[1]);
+            ctx.stroke();
+        }
+        else if(this.currentclick == 2){
+            ctx.rect(0,0,canvas.width,canvas.height);
+            ctx.fill();
+            ctx.stroke();
+            ctx.drawImage(this.itemimg,this.click1[0],this.click1[1],this.click2[0], this.click2[1],10,10, this.click2[0], this.click2[1]);
+
+            debugParagraph.innerHTML = this.click1[0].toString() + "," + this.click1[1].toString() + "," + (this.click2[0]).toString() + "," + (this.click2[1]).toString();
+            this.currentclick = 3;
+        }
+        else{
+			this.spritesArray.push([this.click1[0],this.click1[1],this.click2[0], this.click2[1], canvas.width / 2 - this.tempmousepos.x, canvas.height/2 - this.tempmousepos.y]);
+            this.currentclick = 0;
+            this.drawSpritesheetthingy();
+        }
+    }
+    onmousemove(evt){
+        this.tempmousepos = getMousePos(canvas, evt);
+        ctx.lineWidth = "1";
+        if(this.currentclick == 0){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            this.drawSpritesheetthingy();
+            ctx.beginPath();
+            ctx.rect(this.tempmousepos.x, this.tempmousepos.y, 10, 10);
+            ctx.stroke();
+        }
+        else if(this.currentclick == 1){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            this.drawSpritesheetthingy();
+            ctx.beginPath();
+            ctx.rect(this.click1[0], this.click1[1], this.tempmousepos.x - this.click1[0], this.tempmousepos.y - this.click1[1]);
+            ctx.stroke();
+        }
+        else if(this.currentclick > 2){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(this.itemimg,this.click1[0],this.click1[1],this.click2[0], this.click2[1],this.tempmousepos.x,this.tempmousepos.y, this.click2[0], this.click2[1]);
+
+            ctx.beginPath();
+            ctx.arc(canvas.width / 2, canvas.height / 2, 5, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
+
+    finishUp(){
+        let spriteArrayString = "";
+        this.spritesArray.forEach(sprite =>{
+            spriteArrayString += '['+ sprite +'],';
+        });
+        debugParagraph.innerHTML = '"SkinName": [' + spriteArrayString + ']';
+    }
+}
